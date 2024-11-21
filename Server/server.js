115fecd461938b053e8713fc3f031d8c32ccb484
Abhishek <110390658+Abhishek260305@users.noreply.github.com>
2024-11-22 01:02:31 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 // Set up middleware for JSON parsing and CORS
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://codestandoff.com', 
   methods: ['POST', 'GET'],
 }));
 
